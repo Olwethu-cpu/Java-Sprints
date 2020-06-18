7a62712867ee289b4b76ea3ac37ffadaa0c7bb0d
Tristan Holloway <interncptwex036@capaciti.org.za>
2020-06-18 17:22:28 +0200

    Update sprint3.java
    Added a scanner for user input.

--- Java Sprints/Sprint 3/sprint3.java
+++ Java Sprints/Sprint 3/sprint3.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
+
 public class sprint3{
     public static void main(String[] args) {
         
-        int input = 10;
+        System.out.println("Please enter the amount of rows you want to be printed out.");
 
+        Scanner answer = new Scanner(System.in);
+        int input = answer.nextInt();
+        System.out.println();
         String output = "";
 
         for (int i = 0; i < input; i++) {
             output = output + "*";
             System.out.println(output);
         }
-
+        answer.close();
     }
 }
