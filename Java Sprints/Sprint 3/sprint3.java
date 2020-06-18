701b67300c7d8171b2d0cf61bdcf886ecc2a71c2
Tristan Holloway <interncptwex036@capaciti.org.za>
2020-06-18 18:57:00 +0200

    Update sprint3.java
    Added comments.

--- Java Sprints/Sprint 3/sprint3.java
+++ Java Sprints/Sprint 3/sprint3.java
@@ -3,12 +3,22 @@ import java.util.Scanner;
 public class sprint3{
     public static void main(String[] args) {
         
-        System.out.println("Please enter the amount of rows you want to be printed out.");
+        System.out.println("Please enter the amount of rows that you want printed out.");
 
         Scanner answer = new Scanner(System.in);
         int input = answer.nextInt();
+
+        //if the user enters (input <= 0), the program will stop
+
+        if (input < 1) {
+            System.out.println("Invalid input. Program has been terminated.");
+            System.exit(0);
+        }
+
         System.out.println();
         String output = "";
+
+        //for each iteration, an additional star is added to the output and the printed out
 
         for (int i = 0; i < input; i++) {
             output = output + "*";
