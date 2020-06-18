96543a5d6f640bbf78fa5b2aceae1fb1ce197a2b
Tristan Holloway <interncptwex036@capaciti.org.za>
2020-06-18 16:53:45 +0200

    Update sprint3.java
    Finished the for-loop for the output.

--- Java Sprints/Sprint 3/sprint3.java
+++ Java Sprints/Sprint 3/sprint3.java
@@ -1,5 +1,14 @@
-class sprint3{
+public class sprint3{
     public static void main(String[] args) {
         
+        int input = 10;
+
+        String output = "";
+
+        for (int i = 0; i < input; i++) {
+            output = output + "*";
+            System.out.println(output);
+        }
+
     }
 }
